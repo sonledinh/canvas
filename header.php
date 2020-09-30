0968bec6bf9e23d21dde7a5ebab9f168fdca7eed
SonML <ledinhson209@gmail.com>
2020-09-30 17:28:36 +0700

    update tongkhocanvas.com

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Canvas</title>
+    <!--link css-->
+    <link rel="stylesheet" type="text/css" title="" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/slick.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/slick-theme.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/jquery.mmenu.all.css"> 
+    <link rel="stylesheet" type="text/css" title="" href="css/style.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/responsive.css">
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="ctn-head">
+                    <div class="logo"><a href="index.php"><img src="images/logo.png" class="img-fluid" alt=""></a></div>
+                    <div class="head-right d-flex align-items-center justify-content-center">
+                        <div class="head-search">
+                            <div class="left-top">
+                                <ul class="list-inline d-flex align-items-center">
+                                    <li class="list-inline-item">
+                                        <div class="box-search">
+                                            <input type="text" placeholder="Thỏa mãn nhu cầu trang trí nhà">
+                                            <button type="submit">Tìm kiếm</button>
+                                        </div>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <div class="box-hotline text-center">
+                                            <span>Hotline tư vấn</span>
+                                            <a href="tel:0123456789">0915-800-655</a> 
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="left-bottom">
+                                <ul class="list-inline d-flex justify-content-center align-items-center">
+                                    <li class="list-inline-item"><a href="javascript:void(0)"><i class="fa "></i>các điểm bán hàng</a></li>
+                                    <li class="list-inline-item"><a href="javascript:void(0)"><i class="fa "></i>Sản phẩm yêu thích</a></li>
+                                    <li class="list-inline-item"><a href="javascript:void(0)"><i class="fa "></i>Kiển tra đơn hàng</a></li>
+                                    <li class="list-inline-item"><a href="javascript:void(0)"><i class="fa "></i>Sản phẩm mới</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="head-cart"><a href="" class="text-uppercase"><span>giỏ hàng</span><i class="fa fa-shopping-cart"></i><label>2</label></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ js/private.js
@@ -0,0 +1,19 @@
+
+/*$('.slide-banner').slick({
+    autoplay: false,
+    arrow: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1, 
+    prevArrow: '<button class="prev"><i class="fa fa-chevron-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fa fa-chevron-right"></i></button>',
+});*/
+
+// jQuery(document).ready(function( $ ) {
+//   $("#menu").mmenu({
+//      "extensions": [
+//         "fx-menu-zoom"
+//      ],
+//      "counters": true
+//   });
+// });
